f28ec9a8a0f97e3b4e7612b09d762880bf146125
Zoarchous <zoarchous@gmail.com>
2022-02-13 16:57:48 +0300

    ProMatchesScreen Added

--- app/src/main/java/com/example/dota2stats/presentation/MainActivity.kt
+++ app/src/main/java/com/example/dota2stats/presentation/MainActivity.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.dota2stats.R
 import com.example.dota2stats.domain.proMatches.ProMatchItem
+import com.example.dota2stats.presentation.proMatches.ProMatchesAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -15,23 +17,31 @@ import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerAdapter: ProMatchesAdapter
     private lateinit var viewModel: MainViewModel
     @Inject lateinit var factory: MainViewModelFactory
     private val scope = CoroutineScope(Dispatchers.IO)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_pro_matches)
+        setupRecycler()
         setupViewModel()
-        viewModel.list.observe(this, Observer {
-            Log.d("!!!List", viewModel.list.value.toString())
-        })
-
+        viewModel.list.observe(this) {
+            recyclerAdapter.submitList(it)
+        }
 
     }
     private fun setupViewModel(){
         viewModel = ViewModelProvider(this, factory)[MainViewModel::class.java]
         scope.launch {
             viewModel.getProMatches()
+        }
+    }
+    private fun setupRecycler(){
+        val recycler = findViewById<RecyclerView>(R.id.proMatchesRecView)
+        with(recycler){
+            recyclerAdapter = ProMatchesAdapter()
+            adapter = recyclerAdapter
         }
     }
 }
--- app/src/main/java/com/example/dota2stats/presentation/proMatches/ProMatchesAdapter.kt
+++ app/src/main/java/com/example/dota2stats/presentation/proMatches/ProMatchesAdapter.kt
@@ -1,17 +1,41 @@
 package com.example.dota2stats.presentation.proMatches
 
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
+import com.example.dota2stats.R
+import com.example.dota2stats.R.drawable.ic_android_black_24dp
 import com.example.dota2stats.domain.proMatches.ProMatchItem
 
 class ProMatchesAdapter:
     ListAdapter<ProMatchItem, ProMatchItemViewHolder>(ProMatchItemDiffCallback()) {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProMatchItemViewHolder {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ProMatchItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.pro_match_item,
+            parent,
+            false
+        )
+        return ProMatchItemViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ProMatchItemViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(viewHolder: ProMatchItemViewHolder, position: Int) {
+        val item = getItem(position)
+        viewHolder.league.text = item.league_name
+        viewHolder.direName.text = item.dire_name
+        viewHolder.radiantName.text = item.radiant_name
+        viewHolder.time.text = item.duration.toString()
+        viewHolder.direIcon.setImageResource(ic_android_black_24dp)
+        viewHolder.radiantIcon.setImageResource(ic_android_black_24dp)
+        if (item.radiant_win){
+            viewHolder.direIcon.visibility = View.GONE
+        } else {
+            viewHolder.radiantIcon.visibility = View.GONE
+        }
+
     }
 }
